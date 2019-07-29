e3830674f8927e87a87f4c1f96e593a067adcf45
cyanez <cyanez@ontica.org>
2019-07-28 22:39:51 -0500

    Add Forms

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
@@ -7,18 +8,30 @@ import { CartService } from '../cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
 
   items;
+  checkoutForm;
 
   constructor( 
     private cartService: CartService,
+    private formBuilder: FormBuilder,
     ) { 
       this.items = this.cartService.getItems();
       
+      this.checkoutForm = this.formBuilder.group({
+        name: '',
+        address: ''
+      });
     }
 
-  ngOnInit() {
+  onSubmit(customerData) {
+    //Process checkout data here
+
+    console.warn('Your order has been submitted', customerData);
+
+    this.items =  this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
